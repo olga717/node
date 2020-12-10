7d86f0145fba43e566d149da5f14f18d1edf1781
olga717 <74139091+olga717@users.noreply.github.com>
2020-12-10 15:03:53 +0300

    sort
    Сортировка массива в html

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -1,16 +1,16 @@
-var cars = ["Saab","Volvo", "BMW"];
+/*var cars = ["Saab","Volvo", "BMW"];
 var x = cars[1];
 alert (x);
 
-/*cars[0] = "Ford";
-alert(cars);*/
+cars[0] = "Ford";
+alert(cars);
 alert(cars.length);
 
 cars.push ("Tesla");
 alert(cars);
 
 cars.pop ();
-alert (cars);
+alert (cars);*
 
 var fruits = [ "Lemon", "Orange", "Melone", "Banana"];
 var name = ["Lion", "Jon"];
@@ -21,4 +21,13 @@ var full = all.concat(fruits, name);
 alert(full);
 
 var fullNew = full.slice(8);
-alert(fullNew);
+alert(fullNew);*/
+
+var fruits = [ "Lemon", "Orange", "Melone", "Banana"];
+var name = ["Lion", "Jon"];
+var all = name.concat(fruits);
+
+alert (all);
+
+
+
